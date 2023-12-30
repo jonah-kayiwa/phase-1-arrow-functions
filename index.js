36e36eb5cb31617a01dd24838b1002b682565cde
jonah-kayiwa <kayiwa.jonah@student.moringaschool.com>
2023-12-30 13:41:30 +0300

    ##ARROW FUNCTIONS##

---
+++ index.js
@@ -0,0 +1,7 @@
+//division function//
+const divide = (parameter1, parameter2) => parameter1 / parameter2;
+divide(49,7)
+
+//how many letters each word has//
+const vehicles = ['Bentley','Ferarri','Toyota','Porsche','Mclaren'];
+console.log(vehicles.map((vehicles) => vehicles.length));
